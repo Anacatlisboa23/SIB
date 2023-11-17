d774d738160ceefef1d2295d8f1786b6e49d1a42
Ana Lisboa <102856611+Anacatlisboa23@users.noreply.github.com>
2023-11-17 12:32:35 +0000

    pull

--- src/si/neural_networks/optimizers.py
+++ src/si/neural_networks/optimizers.py
@@ -65,3 +65,80 @@ class SGD(Optimizer):
             self.retained_gradient = np.zeros(np.shape(w))
         self.retained_gradient = self.momentum * self.retained_gradient + (1 - self.momentum) * grad_loss_w
         return w - self.learning_rate * self.retained_gradient
+    #Exercise 15
+
+class Adam(Optimizer):
+    def __init__(self, learning_rate: float, beta_1: float = 0.9, beta_2: float = 0.999, epsilon: float = 1e-8):
+        """
+        Initialize the Adam optimizer.
+
+        Parameters
+        ----------
+        learning_rate: float
+            The learning rate to use for updating the weights.
+        beta_1: float
+            The exponential decay rate for the 1st moment estimates. Defaults to 0.9.
+        beta_2: float
+            The exponential decay rate for the 2nd moment estimates. Defaults to 0.999.
+        epsilon: float
+            A small constant for numerical stability. Defaults to 1e-8.
+        """
+        super().__init__(learning_rate)
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+        self.epsilon = epsilon
+        self.m = None
+        self.v = None
+        self.t = 0
+
+    def update(self, w: np.ndarray, grad_loss_w: np.ndarray) -> np.ndarray:
+        """
+        Update the weights of the layer using the Adam optimization algorithm.
+
+        Parameters
+        ----------
+        w: numpy.ndarray
+            The current weights of the layer.
+        grad_loss_w: numpy.ndarray
+            The gradient of the loss function with respect to the weights.
+
+        Returns
+        -------
+        numpy.ndarray
+            The updated weights of the layer.
+        """
+        if self.m is None:
+            self.m = np.zeros_like(w)
+            self.v = np.zeros_like(w)
+
+        self.t += 1
+        self.m = self.beta_1 * self.m + (1 - self.beta_1) * grad_loss_w
+        self.v = self.beta_2 * self.v + (1 - self.beta_2) * (grad_loss_w ** 2)
+
+        m_hat = self.m / (1 - self.beta_1 ** self.t)
+        v_hat = self.v / (1 - self.beta_2 ** self.t)
+
+        w -= self.learning_rate * m_hat / (np.sqrt(v_hat) + self.epsilon)
+
+        return w
+
+#test
+
+if __name__ == "__main__":
+    # Example weights and gradient
+    initial_weights = np.array([1.0, 2.0, 3.0])
+    gradient = np.array([0.1, 0.2, 0.3])
+
+    # Create an instance of the Adam optimizer
+    adam_optimizer = Adam(learning_rate=0.001)
+
+    # Test Adam optimizer update
+    updated_weights = adam_optimizer.update(initial_weights, gradient)
+
+    # Print results
+    print("Initial Weights:")
+    print(initial_weights)
+    print("Gradient:")
+    print(gradient)
+    print("Updated Weights using Adam:")
+    print(updated_weights)
